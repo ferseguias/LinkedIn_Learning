5dceb080238e11494587d4f6a350068606644e72
ferseguias <ferseg@gmail.com>
2022-08-19 22:57:46 +0200

    sql challenges

---
+++ sql_code_challenges/sql_code_challenges.sql
@@ -0,0 +1,42 @@
+--1
+SELECT created_time, engaged_fans, post_clicks, reach, impressions
+FROM "PostInsights"
+ORDER BY created_time DESC;
+--2
+SELECT "CountryCode", "NumberOfFans"
+FROM "FansPerCountry" AS fpc
+WHERE "Date" = '2018-10-16'
+ORDER BY "NumberOfFans" DESC
+LIMIT 10;
+--3.1
+SELECT DISTINCT date, new_likes
+FROM "GlobalPage"
+ORDER BY new_likes DESC;
+--3.2
+SELECT date, AVG(new_likes)
+FROM "GlobalPage"
+GROUP BY date
+ORDER BY AVG DESC;
+--4
+SELECT date, gender, SUM(number_of_fans), ROUND(SUM(number_of_fans) / (SELECT SUM(number_of_fans) FROM "FansPerGenderAge" WHERE date = '2018-10-16') * 100, 2) AS percentage_per_gender
+FROM "FansPerGenderAge"
+WHERE date = '2018-10-16'
+GROUP BY date, gender
+ORDER BY date;
+--5.1
+SELECT "PopStats"."country_name", ROUND("FansPerCountry"."NumberOfFans" * 100.00 / "PopStats"."population", 2) AS penetration_ratio
+FROM "FansPerCountry"
+JOIN "PopStats"
+ON "FansPerCountry"."CountryCode" = "PopStats"."country_code"
+WHERE "FansPerCountry"."Date" = '2018-10-16'
+ORDER BY penetration_ratio DESC
+LIMIT 10;
+--5.2
+SELECT ps."country_name", ROUND(fpc."NumberOfFans" * 100.00 / ps."population", 2) AS penetration_ratio
+FROM "FansPerCountry" AS fpc
+JOIN "PopStats" AS ps
+ON fpc."CountryCode" = ps."country_code"
+WHERE fpc."Date" = '2018-10-16'
+ORDER BY penetration_ratio DESC
+LIMIT 10;
+
